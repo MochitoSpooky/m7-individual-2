f92b5de7f05d81a0e04620ea2885ac05ddbf504b
MochitoSpooky <119640815+MochitoSpooky@users.noreply.github.com>
2023-06-29 10:25:21 -0400

    clases

--- recordatorio/tareas/models.py
+++ recordatorio/tareas/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Etiqueta(models.Model):
+    nombre = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.nombre
+
+class Tarea(models.Model):
+    ESTADOS_CHOICES = (
+        ('pendiente', 'Pendiente'),
+        ('en_progreso', 'En progreso'),
+        ('completada', 'Completada'),
+    )
+
+    titulo = models.CharField(max_length=100)
+    descripcion = models.TextField()
+    fecha_vencimiento = models.DateField()
+    estado = models.CharField(max_length=20, choices=ESTADOS_CHOICES)
+    etiqueta = models.ForeignKey(Etiqueta, on_delete=models.CASCADE)
+    usuario = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.titulo
+    
+    
